7d1c4ad077ed85019146a8ea67d2ece81ad93e87
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-02-04 01:11:12 +0600

    Initial commit

---
+++ app/src/test/java/com/example/appsd/image_button_animation_zoom/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.appsd.image_button_animation_zoom;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
